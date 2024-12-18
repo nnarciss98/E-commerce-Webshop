9b252a691b890b99c2d07b58a3b1a2c40c2c71b0
NarcisF <36837005+nnarciss98@users.noreply.github.com>
2024-12-18 14:28:04 +0100

    First commit

---
+++ src/test/java/be/codecraft/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package be.codecraft.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
