cef51c5776130ee6e505c288c1178e4f01a76eb5
Toderean R. <raultoderean20@gmail.com>
2025-01-10 14:59:24 +0100

    Change of categories design

--- frontend/src/app/components/home/categories/categories.component.ts
+++ frontend/src/app/components/home/categories/categories.component.ts
@@ -18,48 +18,84 @@ export class CategoriesComponent {
       name: "Matériel d'installation",
       description:
         "Matériaux d'installation : tubes, fixations, interrupteurs, prises, boîtiers.",
-      image: 'assets/images/tools.svg',
+      image: 'assets/images/categories/plug.jpg',
       link: '/category1',
     },
     {
       name: 'Eclairage',
       description:
         'Éclairage LED, sécurité, déco : OSRAM, Philips, LEDVANCE et plus. Pour tous vos projets.',
-      image: 'assets/images/lightbulb.svg',
+      image: 'assets/images/categories/lightbulb.jpg',
       link: '/category2',
     },
     {
       name: 'Energie renouvelable',
       description:
         'HVAC et énergie : Alpha Innotec, Bosch, Kaysun, Ventilair. Panneaux solaires, onduleurs, bornes et batteries.',
-      image: 'assets/images/solar-energy.svg',
+      image: 'assets/images/categories/solar-energy.jpg',
       link: '/category3',
     },
     {
       name: 'Câble',
       description:
         "Câbles d'alimentation, industriels et télécom : INSTALLATIEKABEL, TOPCABLE, PREFLEX SAFE. Qualité et durabilité pour tous vos projets.",
-      image: 'assets/images/tools.svg',
+      image: 'assets/images/categories/cables.jpg',
       link: '/category3',
     },
     {
       name: 'Cheminement de câbles',
       description:
         'Busways, batteries, transformateurs : Italfarad, Schneider, Legrand. Solutions fiables pour transport et distribution d’énergie. Disponibles sur notre e-shop.',
-      image: 'assets/images/tools.svg',
+      image: 'assets/images/categories/lightbulb.jpg',
       link: '/category3',
     },
     {
       name: 'Distribution de courant',
       description:
         'Busways, batteries, transformateurs : Italfarad, Schneider, Legrand. Solutions fiables pour transport et distribution d’énergie. Disponibles sur notre e-shop.',
-      image: 'assets/images/tools.svg',
+      image: 'assets/images/categories/power.jpg',
       link: '/category3',
     },
     {
       name: 'Automatisation du bâtiment',
       description: '',
-      image: 'assets/images/tools.svg',
+      image: 'assets/images/categories/domotique.jpg',
+      link: '/category3',
+    },
+    {
+      name: 'HVAC',
+      description: '',
+      image: 'assets/images/categories/heating.jpg',
+      link: '/category3',
+    },
+    {
+      name: 'Data & Telecom',
+      description: '',
+      image: 'assets/images/categories/telecom.jpg',
+      link: '/category3',
+    },
+    {
+      name: 'Automatisation industrielle',
+      description: '',
+      image: 'assets/images/categories/industrial.jpg',
+      link: '/category3',
+    },
+    {
+      name: 'Outillage',
+      description: '',
+      image: 'assets/images/categories/materiel-installation.jpg',
+      link: '/category3',
+    },
+    {
+      name: 'Électroménager',
+      description: '',
+      image: 'assets/images/categories/electromenager.jpg',
+      link: '/category3',
+    },
+    {
+      name: 'Cheminement de câbles',
+      description: '',
+      image: 'assets/images/categories/routing.jpg',
       link: '/category3',
     },
   ];
