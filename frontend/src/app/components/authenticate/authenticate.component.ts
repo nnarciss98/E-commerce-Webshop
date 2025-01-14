117d212d3d1be51d005defcfea2d794febd0f690
nnarciss98 <nnarciss98@gmail.com>
2025-01-14 11:47:42 +0100

    Added components and routes for authentication

---
+++ frontend/src/app/components/authenticate/authenticate.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+@Component({
+  selector: 'authenticate',
+  standalone: true,
+  imports: [SignInComponent, SignUpComponent],
+  templateUrl: './authenticate.component.html',
+  styleUrl: './authenticate.component.css'
+})
+export class AuthenticateComponent {
+
+}
