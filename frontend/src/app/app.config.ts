b8b66d2cc89b4ba858d5177cf4283296681ec647
nnarciss98 <nnarciss98@gmail.com>
2025-01-21 17:21:31 +0100

    Added translation service for different languages

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,44 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function httpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
+
+export function initializeApp(translate: TranslateService) {
+  return () => {
+    translate.addLangs(['en', 'es']);
+    translate.setDefaultLang('en');
+    return translate.use('en').toPromise();
+  };
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    TranslateStore,
+    provideTranslateService({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [TranslateService],
+      multi: true,
+    },
+  ],
 };
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CategoryService } from '../shop/services/category.service';
 import { Category } from '../types'; // Ensure you have this file with the correct type definition.
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-navbar',
-    imports: [RouterModule, CommonModule],
+    imports: [RouterModule, CommonModule, TranslateModule],
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
@@ -14,7 +15,11 @@ export class NavbarComponent implements OnInit {
   isNavbarOpen = false;
   categories: Category[] = []; // Hold the fetched categories here
 
-  constructor(private categoryService: CategoryService) {}
+  constructor(private categoryService: CategoryService, private translate: TranslateService) {}
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
 
   ngOnInit(): void {
     // Fetch all categories when the component initializes
